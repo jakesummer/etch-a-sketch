5bcebdbc36fef699909a17f61229e8bac04a401a
jakesummer <jakersummer@gmail.com>
2026-03-21 07:27:42 -0400

    Make squares change to a random color when hovered

--- script.js
+++ script.js
@@ -3,7 +3,21 @@ let gridContainer = document.querySelector("#grid-container");
 for (let i = 0; i < 16; i++) {
     for (let j = 0; j < 16; j++) {
         let square = document.createElement("div");
-        square.style.backgroundColor = "blue";
+        square.style.backgroundColor = "black";
+
+        square.addEventListener("mouseover", (e) => {
+            e.target.style.backgroundColor === "black" ?
+            e.target.style.backgroundColor = getRandomColor():
+            "";
+        })
+
         gridContainer.appendChild(square);
     }
+}
+
+function getRandomColor() {
+    const hue = Math.floor(Math.random() * 360);
+    const saturation = 100
+    const lightness = 50;
+    return `hsl(${hue}, ${saturation}%, ${lightness}%)`
 }
