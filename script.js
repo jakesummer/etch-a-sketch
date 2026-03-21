b0a98b446546a7b37875f6e28e089d4493404597
jakesummer <jakersummer@gmail.com>
2026-03-21 08:15:41 -0400

    Allow user to change grid size and update grid

--- script.js
+++ script.js
@@ -1,18 +1,44 @@
 let gridContainer = document.querySelector("#grid-container");
+let changeGridSizeButton = document.querySelector("#change-size");
 
-for (let i = 0; i < 16; i++) {
-    for (let j = 0; j < 16; j++) {
-        let square = document.createElement("div");
-        square.style.backgroundColor = "black";
+changeGridSizeButton.addEventListener("click", changeGridSize)
 
-        square.addEventListener("mouseover", (e) => {
-            e.target.style.backgroundColor === "black" ?
-            e.target.style.backgroundColor = getRandomColor():
-            "";
-        })
+function createGrid(squaresPerSide){
+    for (let i = 0; i < squaresPerSide; i++) {
+        for (let j = 0; j < squaresPerSide; j++) {
+            let square = document.createElement("div");
 
-        gridContainer.appendChild(square);
+            let squareSize = `calc(100% / ${squaresPerSide})`;
+            square.style.cssText = `width: ${squareSize}; height: ${squareSize}; `;
+
+            square.style.backgroundColor = "black";
+
+            square.addEventListener("mouseover", (e) => {
+                e.target.style.backgroundColor === "black" ?
+                e.target.style.backgroundColor = getRandomColor():
+                "";
+            })
+
+            gridContainer.appendChild(square);
+        }
     }
+}
+
+function changeGridSize(){
+    let newGridSize;
+
+    while (!(newGridSize >= 1 && newGridSize <= 100)) {
+        newGridSize = prompt("Enter squares per side (1-100):");
+
+        if (!newGridSize) return;
+    }
+    removeGrid();
+    createGrid(newGridSize);
+    alert(`Squares per side changed to ${newGridSize}`);
+}
+
+function removeGrid() {
+    gridContainer.replaceChildren();
 }
 
 function getRandomColor() {
@@ -21,3 +47,5 @@ function getRandomColor() {
     const lightness = 50;
     return `hsl(${hue}, ${saturation}%, ${lightness}%)`
 }
+
+createGrid(16);
